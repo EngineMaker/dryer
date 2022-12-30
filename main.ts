6d4f27c2165a0c6c502ac812fba33523d7c8453f
mktoho <mk@mktoho.org>
2022-12-30 10:27:11 +0900

    こうか

--- main.ts
+++ main.ts
@@ -4,8 +4,7 @@ import { callTuyaAPI } from './tuya-api.ts'
 
 serve(async req => {
   const { TUYA_CLIENT_ID, TUYA_CLIENT_SECRET } =
-    (await load()) || Deno.env.toObject()
-  console.log({ env: Deno.env.toObject() })
+    (await load()) || Deno.env.toObject().env
   if (req.method === 'GET') {
     return new Response('BAD REQEUST', { status: 400 })
   }
